957aec4a13eac0051536b39fde6029f49d139fba
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-02-04 19:29:39 -0500

    structure of typing game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { lorem } from 'faker';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'typing';
+export class AppComponent implements OnInit {
+  randomText =  lorem.sentence();
+  enteredText  = '';
+
+  ngOnInit(){
+    this.randomText;
+  }
+
+  onInput(value: string){
+    this.enteredText = value;
+    console.log(value);
+  }
+
 }
